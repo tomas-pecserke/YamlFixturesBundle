11f18d6978b420d4d3884dba7aa3b0eb9d313607
Tomáš Pecsérke <tomas@pecserke.eu>
2013-01-23 03:46:42 +0100

    added test & marked incomplete for now

---
+++ Tests/Command/LoadFixturesCommandTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Publero\YamlFixturesBundle\Tests\Command;
+
+class LoadFixturesCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        $this->markTestIncomplete();
+    }
+}
