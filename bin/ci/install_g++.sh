768ea93e583e5b77d39ef8424304746d7c93629a
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2014-12-03 05:52:15 +0100

    testing hhvm ext-mongo compilation #4 - update-alternatives & conditional run

---
+++ bin/ci/install_g++.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+#
+# install g++ 4.8
+#
+# Run as sudo
+
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update -qq
+apt-get install -qq hhvm-dev g++-4.8
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
