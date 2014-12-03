b8ff3746046977a2e452b235a44adc8d5ea0bf53
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2014-12-03 06:03:02 +0100

    testing hhvm ext-mongo compilation #6 - dependency

--- bin/ci/install_g++.sh
+++ bin/ci/install_g++.sh
@@ -4,7 +4,18 @@
 #
 # Run as sudo
 
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get update -qq
-apt-get install -qq hhvm-dev g++-4.8 libbson
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -qq hhvm-dev g++-4.8 git-core automake autoconf libtool gcc
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+
+# compile libbson
+wget https://github.com/mongodb/libbson/archive/master.tar.gz
+tar xzf master.tar.gz
+rm master.tar.gz
+cd libbson-master
+./configure
+make
+sudo make install
+cd ..
+rm libbson-master -r
