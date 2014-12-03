6650aeaefe502c6d3d5c8d8742af84d0d2aeb30f
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2014-12-03 06:45:28 +0100

    testing hhvm ext-mongo compilation #9 - libgoogle-glog-dev

--- bin/ci/install_g++.sh
+++ bin/ci/install_g++.sh
@@ -9,6 +9,14 @@ sudo apt-get update -qq
 sudo apt-get install -qq hhvm-dev g++-4.8 git-core automake autoconf libtool gcc libboost-dev libgoogle-glog-dev
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
+# install libgoogle.log-dev
+wget http://launchpadlibrarian.net/80433359/libgoogle-glog0_0.3.1-1ubuntu1_amd64.deb
+sudo dpkg -i libgoogle-glog0_0.3.1-1ubuntu1_amd64.deb
+rm libgoogle-glog0_0.3.1-1ubuntu1_amd64.deb
+wget http://launchpadlibrarian.net/80433361/libgoogle-glog-dev_0.3.1-1ubuntu1_amd64.deb
+sudo dpkg -i libgoogle-glog-dev_0.3.1-1ubuntu1_amd64.deb
+rm libgoogle-glog-dev_0.3.1-1ubuntu1_amd64.deb
+
 # compile libbson
 wget https://github.com/mongodb/libbson/archive/master.tar.gz
 tar xzf master.tar.gz
