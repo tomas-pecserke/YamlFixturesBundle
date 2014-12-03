b98e3cac586fb439e99fbdde844aff1821eda6de
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2014-12-03 05:56:39 +0100

    testing hhvm ext-mongo compilation #6 - dependency

--- bin/ci/install_g++.sh
+++ bin/ci/install_g++.sh
@@ -6,5 +6,5 @@
 
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update -qq
-apt-get install -qq hhvm-dev g++-4.8
+apt-get install -qq hhvm-dev g++-4.8 libbson
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
