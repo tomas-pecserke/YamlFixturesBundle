640d9a61c2685c949a12f7b148e3311fc5ec17f3
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-05-06 21:04:38 +0200

    added tests

--- Tests/DataFixtures/YamlFixtureFileParserTest.php
+++ Tests/DataFixtures/YamlFixtureFileParserTest.php
@@ -1,11 +1,19 @@
 <?php
 namespace Pecserke\YamlFixturesBundle\Tests\DataFixtures;
 
-use Pecserke\YamlFixturesBundle\DataFixtures\Exception\InvalidFixturesException;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use org\bovigo\vfs\vfsStreamWrapper;
 use Pecserke\YamlFixturesBundle\DataFixtures\YamlFixtureFileParser;
 
 class YamlFixtureFileParserTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        vfsStreamWrapper::register();
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('testDir'));
+    }
+
     public function testParse()
     {
         $file = __DIR__ . '/../Fixtures/AcmeDemoBundle/Resources/fixtures/example_1.yml';
@@ -44,65 +52,60 @@ class YamlFixtureFileParserTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @expectedException \Pecserke\YamlFixturesBundle\DataFixtures\Exception\InvalidFixturesException
-     * @expectedExceptionMessage file 'non_existent_file' does not exist
+     * @expectedExceptionMessageRegExp  /^file '.*' does not exist$/
      */
     public function testParseThrowsInvalidFixturesOnFileDoesNotExist()
     {
+        $file = vfsStream::url('testDir/non_existent_file');
         $parser = new YamlFixtureFileParser();
-        $parser->parse(array(
-            'non_existent_file'
-        ));
+        $parser->parse(array($file));
     }
 
     /**
      * @expectedException \Pecserke\YamlFixturesBundle\DataFixtures\Exception\InvalidFixturesException
-     * @expectedExceptionMessage cannot read file 'not_readable_file'
+     * @expectedExceptionMessageRegExp /^cannot read file '.*'$/
      */
     public function testParseThrowsInvalidFixturesOnFileNotReadable()
     {
-        try {
-            file_put_contents('not_readable_file', '');
-            chmod('not_readable_file', 0222);
-            $parser = new YamlFixtureFileParser();
-            $parser->parse(array('not_readable_file'));
-            unlink('not_readable_file');
-        } catch (InvalidFixturesException $e) {
-            unlink('not_readable_file');
-            throw $e;
-        }
+        $file = vfsStream::url('testDir/not_readable_file');
+        file_put_contents($file, '');
+        chmod($file, 0222);
+        $parser = new YamlFixtureFileParser();
+        $parser->parse(array($file));
     }
 
     /**
      * @expectedException \Pecserke\YamlFixturesBundle\DataFixtures\Exception\InvalidFixturesException
-     * @expectedExceptionMessage no fixture configuration found in file 'empty_file', fix this by adding fixture configuration to file or by removing the file
+     * @expectedExceptionMessageRegExp /^no fixture configuration found in file '.*', fix this by adding fixture configuration to file or by removing the file$/
      */
     public function testParseThrowsInvalidFixturesOnEmptyFile()
     {
-        try {
-            file_put_contents('empty_file', '');
-            $parser = new YamlFixtureFileParser();
-            $parser->parse(array('empty_file'));
-            unlink('empty_file');
-        } catch (InvalidFixturesException $e) {
-            unlink('empty_file');
-            throw $e;
-        }
+        $file = vfsStream::url('testDir/empty_file');
+        file_put_contents($file, '');
+        $parser = new YamlFixtureFileParser();
+        $parser->parse(array($file));
     }
 
     /**
      * @expectedException \Pecserke\YamlFixturesBundle\DataFixtures\Exception\InvalidFixturesException
-     * @expectedExceptionMessageRegExp  /^fixtures in file 'incorrect_data', must be defined as array. Data given: /
+     * @expectedExceptionMessageRegExp  /^fixtures in file '.*', must be defined as array. Data given: /
      */
     public function testParseThrowsInvalidFixturesOnNotArrayFixtures()
     {
-        try {
-            file_put_contents('incorrect_data', '1');
-            $parser = new YamlFixtureFileParser();
-            $parser->parse(array('incorrect_data'));
-            unlink('incorrect_data');
-        } catch (InvalidFixturesException $e) {
-            unlink('incorrect_data');
-            throw $e;
-        }
+        $file = vfsStream::url('testDir/incorrect_data');
+        file_put_contents($file, '1');
+        $parser = new YamlFixtureFileParser();
+        $parser->parse(array($file));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessageRegExp  /^class '.*' doesn't exist in file '.*'$/
+     */
+    public function testParseClassNotExists() {
+        $file = vfsStream::url('testDir/testFile.yml');
+        file_put_contents($file, 'ThisClassDoesNotExists: []');
+        $parser = new YamlFixtureFileParser();
+        $parser->parse(array($file));
     }
 }
